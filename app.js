39c23fe075b58a36de39d1f02e2054b5087e603a
Abbosjon Madiev <abbosjon.madiev@gmail.com>
2023-07-21 18:21:33 +0300

    boilerplate template

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const compression = require("compression");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+// Additional utils
+const globalErrorHandler = require("./controllers/errorController");
+const UserRouter = require("./routes/UserRoutes");
+const adminRouter = require("./routes/adminRoutes");
+const LinksRouter = require("./routes/LinksRoutes");
+
+// 1) INIT FUNCTIONS
+const app = express();
+app.enable("trust proxy");
+
+// Swagger Doc
+app.use("/", swaggerUI.serve);
+app.get("/", swaggerUI.setup(swaggerDocument));
+
+// 2) BADY PARSERS
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(compression());
+
+// 3) ROUTES
+app.use("/api/v1/user", UserRouter);
+app.use("/api/v1/admin", adminRouter);
+app.use("/api/v1/link", LinksRouter);
+
+// app.all("*", (req, res, next) => {
+//     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+//   });
+
+app.use(globalErrorHandler);
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose").set("strictQuery", false);
+const dotenv = require("dotenv");
+const path = require("path");
+const app = require("./app");
+
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: path.resolve(__dirname, "./config.env") });
+
+const port = process.env.APP_PORT || 8000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}....`);
+});
+
+const DB = process.env.DB_DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB connection successful!"));
+
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("SIGTERM", () => {
+  console.log("👋 SIGTERM RECEIVED. Shutting down gracefully");
+  server.close(() => {
+    console.log("💥 Process terminated!");
+  });
+});
